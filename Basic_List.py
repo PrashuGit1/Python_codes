d9dbba52cf91207a96f8a116bb5ec6450063ec75
PrashuGit1 <shukla2018prakash@gmail.com>
2025-03-28 09:54:46 +0530

    This is new file

---
+++ Basic_List.py
@@ -0,0 +1,32 @@
+#list can store any type of data
+
+li=[1,2,3,1,3,1,4,5]
+"""print(li)
+print(li[0], li[-1])
+
+li[2]=9
+print(li)
+
+# List Methods
+
+li.append(13)
+print(li)
+
+l1=li.copy()
+print(l1)
+
+l1.clear()
+print(l1)
+
+print(li.count(1))
+
+l2=[11,56,23]
+li.extend(l2)
+print(li)
+
+print(li.index(3))"""
+
+#insert
+
+li.insert(3, 10)
+print(li)
